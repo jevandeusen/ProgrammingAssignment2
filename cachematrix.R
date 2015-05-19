037af21e7e19db24f9df614ffd040e2b5f6d1ef2
John Van Deusen <jvandeus@gmail.com>
2015-05-19 13:17:58 -0600

    Implemented functionality: makeCacheMatrix(x) - returns a list of fuctions, (getX, setX, cacheSolve). 	Caches x matrix in the parent environment:   1: getX() is used to return the original vector, (cmX).   2: setX() sets the value of x, even if it has not changed, and sets the 	inverse of x (cmI) to NULL.   3: cacheSolve() returns the inverse of cmX. If it has been previously called 	it will return the cached value of the inverse; otherwise it will 	solve for the inverse and cache it.
    cacheSolveList is a list of instances of the makeCacheMatrix() call that
    	exists in the global environment. If it does not exist or is null
    	it is created by the cacheSolve() function to have a length of 5.
    cacheSolve(x) - a different form of cacheSolve() than the one returned in
    	the makeCacheMatrix() call. This returns the inverse of the
    	invertible square matrix x.
    	Instances of the makeCAcheMatrix(x) call are stored in the
    	available slots of the cacheSolveList. Once it is full,
    	subsequent calls are placed at the end of the list and the
    	oldest instance is deleted.
    cacheSolveClear() sets the value of the cacheSolveList to NULL.
    	makeCacheMatrix(x) call in available

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## cachematrix.R
+## Coursera rprog-014
+## ProgrammingAssignment2
+## John Van Deusen
+## makeCacheMatrix(...) takes a single square matrix x and returns a list of functions.
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+     cmX <- x
+     cmI <- NULL
+     
+     ## getX() - returns the stored x matrix
+     getX <- function() {
+          cmX
+     }
+     
+     ## setX(x) Sets the value of the stored matrix cmX and sets its inverse to NULL
+     setX <- function(x, cmL) {
+          cmX <<- x
+          cmI <<- NULL
+     }
+     
+     ## cacheSolve() returns the inverse of the makeCacheMatrix() instance. It will either return
+     ##   a cached copy of the inverse or the solved inverse depending upon whether casheSolve() has
+     ##   been previously invoked.
+     cacheSolve <- function() {
+          if (is.null(cmI)) {
+               cmI <<- solve(cmX)
+          }
+          cmI
+     }
+     ## The list returned by the makeCacheMatrix(x) call.
+     list(getX=getX, setX=setX, cacheSolve=cacheSolve)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve(x, ...) takes a square, invertible matrix x, and returns the inverse.
+##   If cacheSolve(x) has been invoked previously with a matrix identical to x, the a cached copy
+##   of the inverse of x is returned.
+## A number of matrices can be passed to cacheSolve() and it will cache the inverses for all of them.
+##   This is done by accessing a linked list of instances of the makeCacheMatrix(x) call.
+##   A single variable holding the instance(s) of the makeCacheMatrix(x) call, cacheSolveList,
+##   is stored in the global environment.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## The size of the cacheSolveList is the maximum number of inverses that will be stored.
+     ##   Additional matricies will cause the oldest to be overwritten.
+     if (!exists("cacheSolveList") || is.null(cacheSolveList)) {
+          cacheSolveList <<- vector("list", 5)
+     }
+     index <- 1
+     while (!is.null(cacheSolveList[[index]])) {
+          if (identical(x, cacheSolveList[[index]]$getX())) {
+               return(cacheSolveList[[index]]$cacheSolve())
+          }
+          ## if index exceeds the length of cacheSolveList delete the oldest
+          if (index >= length(cacheSolveList)) {
+               cacheSolveList[[1]] <<- NULL
+               break
+          } else {
+               index <- index + 1
+          }
+     }
+     cacheSolveList[[index]] <<- makeCacheMatrix(x)
+     cacheSolveList[[index]]$cacheSolve()    
+}
+## cacheSolveClear() sets the instance of the cacheSolveList variable in the global environment to NULL.
+cacheSolveClear <- function() {
+     if (exists("cacheSolveList")) { cacheSolveList <<- NULL }
 }
